5998b06b52384fb15f30f82b009647e4c0b9574b
ProFessor <122669733+itsprofessorX@users.noreply.github.com>
2024-09-22 19:10:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,11 @@ rm -rf .repo/local_manifests
 
 #sync
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b master .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
-  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 fi
 
 
