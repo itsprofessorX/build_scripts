ec22d342dad4a8d03a9402d8122a3cde04c5debe
ProFessor <122669733+itsprofessorX@users.noreply.github.com>
2024-09-23 09:10:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
 git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b master .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
-  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 fi
 
 
@@ -17,4 +17,4 @@ fi
 . build/envsetup.sh
 lunch lineage_r5x-ap2a-userdebug
 mka installclean
-mka bacon
+m evolution
