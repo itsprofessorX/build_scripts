00a9e5579a488687182191c59561d21867acbcb8
ProFessor <122669733+itsprofessorX@users.noreply.github.com>
2024-10-01 17:44:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
 git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b master .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
-  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+  repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 fi
 
 
 #build
 . build/envsetup.sh
-lunch lineage_r5x-userdebug
+goafterlife r5x
 mka installclean
-m evolution
+mka bacon
