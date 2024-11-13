936469772feb7008fec6d8231890009d7dd9f6f4
ProFessor <122669733+itsprofessorX@users.noreply.github.com>
2024-11-13 13:29:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,15 +4,17 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
+repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
 git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b master .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
-  repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 fi
 
 
 #build
 . build/envsetup.sh
-goafterlife r5x
+riseup r5x userdebug
+rise b
+  
