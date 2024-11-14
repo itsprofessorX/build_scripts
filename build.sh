cf23558c94415f23d86f6d18bea514b334743ae0
ProFessor <122669733+itsprofessorX@users.noreply.github.com>
2024-11-14 22:11:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,17 +4,18 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
+repo init -u https://github.com/GenesisOS/manifest.git -b verve --git-lfs
 git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b master .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
-  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+  repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune --optimized-fetch
 fi
 
 
 #build
-. build/envsetup.sh
-riseup r5x userdebug
-rise b
+source build/envsetup.sh
+breakfast r5x
+mka installclean
+mka genesis
   
