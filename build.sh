3a505d3f49e6b60c8bfe51c0031e0c5f6ff85d7d
ProFessor <122669733+itsprofessorX@users.noreply.github.com>
2024-11-14 22:13:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf .repo/local_manifests
 
 #sync
 repo init -u https://github.com/GenesisOS/manifest.git -b verve --git-lfs
-git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b GOS .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
