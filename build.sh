b719deaa28f22faf9a383144aaccebe0d9a6f381
ProFessor <122669733+itsprofessorX@users.noreply.github.com>
2024-11-17 13:00:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,17 +4,18 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
+repo init -u https://github.com/PixelOS-Fifteen/manifest.git -b fifteen --git-lfs --depth=1
 git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b master .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
-  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 fi
 
 
 #build
 . build/envsetup.sh
-riseup r5x userdebug
-rise b
+lunch aosp_r5x-ap3a-user
+mka installclean
+mka bacon
   
