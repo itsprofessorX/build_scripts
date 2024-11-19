66d55929143242590e8143b9bc40f787538237dc
ProFessor <122669733+itsprofessorX@users.noreply.github.com>
2024-11-19 21:32:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf .repo/local_manifests
 
 #sync
 repo init -u https://github.com/PixelOS-Fifteen/manifest.git -b fifteen --git-lfs --depth=1
-git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/itsprofessorX/local_manifest.git --depth 1 -b POS .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
