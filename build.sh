d7f23b7a824c2b8afb3a28b5c399133af2159b81
Professor <122669733+itsprofessorX@users.noreply.github.com>
2025-02-09 00:52:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,18 +4,17 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs --depth=1
-git clone https://github.com/itsprofessorX/local_manifest.git -b master .repo/local_manifests
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs --depth=1
+git clone https://github.com/itsprofessorX/local_manifest.git -b Pos .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
-  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+  repo sync
 fi
 
-export PIXELAGE_BUILD=r5x
 
 #build
 . build/envsetup.sh
-lunch pixelage_r5x-ap4a-userdebug
+lunch aosp_r5x-ap4a-userdebug
 mka installclean
 mka bacon
